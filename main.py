a796109548cd35880269403247285406e54bb6e6
AJ <amariusj.lesure@gmail.com>
2026-03-19 12:03:38 -0500

    Add first FastAPI method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from fastapi-blog!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def home():
+    return {"message": "Hello Wdorl!"}
