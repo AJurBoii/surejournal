b3e6977eef26c1fe1f99b21c307e79106f108b25
AJ <amariusj.lesure@gmail.com>
2026-03-21 21:31:53 -0500

    Create JournalEntry object

--- entries.py
+++ entries.py
@@ -1,6 +1,37 @@
 from datetime import datetime
 
 
-def create_entry(user: str, content: str) -> dict:
-    entry = {"user": user, "datetime": datetime.now().strftime("%m/%d/%Y, %I:%M %p")}
-    return entry
+class JournalEntry:
+    def __init__(self, user: str, content: str):
+        self.user = user
+        self.content = content
+        self.timestamp = [datetime.now().strftime("%m-%d-%Y, %I:%M %p")]
+
+    def editEntry(self, content: str):
+        self.content = content
+        self.timestamp.append(datetime.now().strftime("%m-%d-%Y, %I:%M %p"))
+
+    def getEntry(self) -> str:
+        return self.content
+
+    def getUser(self) -> str:
+        return self.user
+
+    def getTimestamp(self) -> str:
+        return (
+            "Posted "
+            + self.timestamp[len(self.timestamp) - 1]
+            + " (last edited "
+            + self.timestamp[len(self.timestamp) - 1]
+            + ")"
+            if len(self.timestamp) > 1
+            else "Posted " + self.timestamp[0]
+        )
+
+
+testEntry = JournalEntry("aj.lesure", "This is a test entry.")
+print(testEntry.getEntry())
+print(testEntry.getUser())
+print(testEntry.getTimestamp())
+testEntry.editEntry("The content of the entry has changed.")
+print(testEntry.getTimestamp())
