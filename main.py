264b9525f4b43603903cd849984bc34c31d2e4cf
AJ <amariusj.lesure@gmail.com>
2026-03-23 10:17:39 -0500

    Import and use JournalEntry objects

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
+from entries import JournalEntry
+
 app = FastAPI(
     version="0.1.0",
     title="SureJournal API",
@@ -12,25 +14,22 @@ app = FastAPI(
 
 templates = Jinja2Templates(directory="templates")
 
-entries: list[dict] = [
-    {
-        "user": "aj.lesure",
-        "id": 0,
-        "date": datetime(2026, 3, 19, 13, 4, 0).strftime("%m-%d-%Y, %I:%M %p"),
-        "content": "Today I felt a little anxious to learn FastAPI, but I know everything will work out as long as I pace myself and have confidence.",
-    },
-    {
-        "user": "aj.lesure",
-        "id": 1,
-        "date": datetime(2026, 3, 19, 13, 11, 0).strftime("%m-%d-%Y, %I:%M %p"),
-        "content": "I just learned how to create datetime objects and format them into strings. I feel pretty good about that!",
-    },
-    {
-        "user": "random.person",
-        "id": 2,
-        "date": datetime.now().strftime("%m-%d-%Y, %I:%M %p"),
-        "content": "Hello, I am a random person using this journaling app to write about my feelings and keep track of my mood. It's helpful!!",
-    },
+entries: list[JournalEntry] = [
+    JournalEntry(
+        user="aj.lesure",
+        content="Today I felt a little anxious to learn FastAPI, but I know everything will work out as long as I pace myself and have confidence.",
+        timestamp=datetime(2026, 3, 19, 13, 4, 0),
+    ),
+    JournalEntry(
+        user="aj.lesure",
+        content="I just learned how to create datetime objects and format them into strings. I feel pretty good about that!",
+        timestamp=datetime(2026, 3, 19, 13, 11, 0),
+    ),
+    JournalEntry(
+        user="random.person",
+        content="Hello, I am a random person using this journaling app to write about my feelings and keep track of my mood. It's helpful!!",
+        timestamp=datetime.now(),
+    ),
 ]
 
 
