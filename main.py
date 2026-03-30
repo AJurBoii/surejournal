43748769ad4981b120eb19967900f289f879b3aa
AJ <amariusj.lesure@gmail.com>
2026-03-30 13:13:55 -0500

    Remove # ignore for imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from fastapi import FastAPI, Request # type: ignore
-from fastapi.templating import Jinja2Templates #type: ignore
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 from entries import JournalEntry
 
